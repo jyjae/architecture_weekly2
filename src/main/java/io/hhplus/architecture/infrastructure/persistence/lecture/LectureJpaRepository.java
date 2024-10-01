3a6574c25c254f5b11a5ab3c68773c7165ad732d
Yeonjaery <yeonjaery@gmail.com>
2024-10-01 17:56:21 +0900

    Fix: 수강 신청 가능한 강의 조회 쿼리 작성

--- src/main/java/io/hhplus/architecture/infrastructure/persistence/lecture/LectureJpaRepository.java
+++ src/main/java/io/hhplus/architecture/infrastructure/persistence/lecture/LectureJpaRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface LectureJpaRepository extends JpaRepository<LectureJpaEntity, Long> {
     @Query("SELECT l FROM LectureJpaEntity l WHERE (:lectureIds IS NULL OR l.id NOT IN :lectureIds) AND l.startTime > :startTime")
